561bddde970ad1a714f1275d01861078dc3da533
dpolozov <dpolozov@gmail.com>
2020-07-02 12:04:50 -0700

    buttons and navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import ComponentsScreen from "./CompenentsScreen";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Something!</Text>;
+const HomeScreen = ({navigation}) => {
+  
+  return (
+    <View>
+      <Text style={styles.text}>React Native Demo</Text>
+      <Button 
+        title='Go to Components Demo'
+        onPress={() => navigation.navigate('Components')}
+      />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
+      {/*
+      <TouchableOpacity onPress={() => navigation.navigate('List')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+      */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
